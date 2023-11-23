9619d7202c77eef7fa9d1f9127ab5f369ad0710c
halimturan <halimturan61@gmail.com>
2023-11-23 22:17:05 +0300

    first commit

--- hegem_vakfi/settings.py
+++ hegem_vakfi/settings.py
@@ -159,7 +159,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
