1c08d262acdf2394fdadfd203e5ce0ddbbccbd13
halimturan <halimturan61@gmail.com>
2023-11-24 13:43:40 +0300

    first commit

--- apps/site_settings/views.py
+++ apps/site_settings/views.py
@@ -4,8 +4,8 @@ from apps.site_settings.models import SocialMediaSettings, SiteSettings, Menu
 def context(request):
     if 'super/user' in request.path:
         return {}
-    header_menu = Menu.objects.filter(menu_location__name="navbar").order_by('alignment')
-    footer_menu = Menu.objects.filter(menu_location__name="footer").order_by('alignment')
+    header_menu = Menu.objects.filter(menu_location__name="navbar")
+    footer_menu = Menu.objects.filter(menu_location__name="footer")
     social_media_settings = SocialMediaSettings.objects.last()
     site_settings = SiteSettings.objects.last()
     return {
